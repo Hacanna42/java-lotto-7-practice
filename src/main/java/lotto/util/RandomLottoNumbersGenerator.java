ed24692dd954f602f6cd0b63cc27a6ab186c02be
Hacanna42 <dtmdgus901@gmail.com>
2024-11-29 00:20:01 +0900

    refactor: 로또 번호 생성 메서드의 생성자를 삭제

--- src/main/java/lotto/util/RandomLottoNumbersGenerator.java
+++ src/main/java/lotto/util/RandomLottoNumbersGenerator.java
@@ -4,18 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class RandomLottoNumbersGenerator implements LottoNumbersGenerator {
-    private static final int LOTTO_NUMBERS_COUNT = 6;
-
-    private final int startInclusive;
-    private final int endInclusive;
-
-    public RandomLottoNumbersGenerator(int startInclusive, int endInclusive) {
-        this.startInclusive = startInclusive;
-        this.endInclusive = endInclusive;
-    }
-
     @Override
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(startInclusive, endInclusive, LOTTO_NUMBERS_COUNT);
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
 }
