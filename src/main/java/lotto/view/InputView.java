95dd4dcfd63eb8af04b9b2bf105303837603bdc2
Hacanna42 <dtmdgus901@gmail.com>
2024-11-28 23:15:29 +0900

    refactor(InputView): 메서드명 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,7 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getBuyAmount() {
+    public static String getPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         return Console.readLine();
     }
