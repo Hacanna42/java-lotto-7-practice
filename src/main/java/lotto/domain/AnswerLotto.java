b45c594d4468d0cfed14a0000aed76c3b9b4fded
Hacanna42 <dtmdgus901@gmail.com>
2024-11-29 20:30:21 +0900

    feat(AnswerLotto): 검증 기능 등 추가

--- src/main/java/lotto/domain/AnswerLotto.java
+++ src/main/java/lotto/domain/AnswerLotto.java
@@ -1,11 +1,24 @@
 package lotto.domain;
 
+import lotto.dto.AnswerNumbersDto;
+
 public class AnswerLotto {
     private final Lotto answerLotto;
     private final int bonusNumber;
 
     public AnswerLotto(Lotto answerLotto, int bonusNumber) {
+        validate(answerLotto, bonusNumber);
         this.answerLotto = answerLotto;
         this.bonusNumber = bonusNumber;
+    }
+
+    public AnswerNumbersDto getAnswerNumbersDto() {
+        return new AnswerNumbersDto(answerLotto.getLottoNumbersDto().numbers(), bonusNumber);
+    }
+
+    private void validate(Lotto answerLotto, int bonusNumber) {
+        if (answerLotto.isContain(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] ...");
+        }
     }
 }
