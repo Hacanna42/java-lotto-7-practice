41693a6098ab94119d3eae8ed3d470fc83d7ee10
Hacanna42 <dtmdgus901@gmail.com>
2024-11-29 20:33:21 +0900

    feat(Parser): AnswerLotto 객체를 반환하는 메서드 추가

--- src/main/java/lotto/util/Parser.java
+++ src/main/java/lotto/util/Parser.java
@@ -1,9 +1,19 @@
 package lotto.util;
 
+import java.util.Arrays;
+import java.util.List;
+import lotto.domain.AnswerLotto;
+import lotto.domain.Lotto;
 import lotto.domain.Money;
 
 public class Parser {
     public static Money parseMoney(String amount) {
         return Money.initFrom(Integer.parseInt(amount));
+    }
+
+    public static AnswerLotto parseAnswerLotto(String answerLottoNumbers, String answerBonusNumber) {
+        List<Integer> lottoNumbers = Arrays.stream(answerLottoNumbers.split(",")).map(Integer::parseInt).toList();
+        Lotto lotto = new Lotto(lottoNumbers);
+        return new AnswerLotto(lotto, Integer.parseInt(answerBonusNumber));
     }
 }
